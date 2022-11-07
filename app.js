61f707232d6e7b66be751e4ed523b05ef340848d
SunnyAccu <30sunny03yadav2002@gmail.com>
2022-11-08 00:10:17 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+require('express-async-errors')
+// express
+const express=require('express')
+const app=express()
+// rest of the package
+const morgan=require('morgan')
+const cookieParser=require('cookie-parser')
+const fileUpload=require('express-fileupload')
+
+// database
+const connectDB=require('./db/connect')
+
+// routers
+const authRouter=require('./routes/authRoutes')
+const userRouter=require('./routes/userRoutes')
+const productRouter=require('./routes/productRoutes')
+const reviewRouter=require('./routes/reviewRoutes')
+const orderRouter=require('./routes/orderRoutes')
+
+// middleware
+const notFoundMiddlware=require('./middleware/not-found')
+const errorHandlerMiddlware=require('./middleware/error-handler')
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+app.use(express.static('./public'))
+app.use(fileUpload())
+
+app.get('/',(req,res)=>{
+    console.log(req.signedCookies)
+    res.send('e commerce api')
+})
+
+app.use('/api/v1/auth',authRouter)
+app.use('/api/v1/users',userRouter)
+app.use('/api/v1/products',productRouter)
+app.use('/api/v1/reviews',reviewRouter)
+app.use('/api/v1/orders',orderRouter)
+
+app.use(notFoundMiddlware)
+app.use(errorHandlerMiddlware)
+
+
+const port=process.env.PORT
+
+
+
+const start=async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`the server is running at ${port}`))
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+start()
+
+
+
---
+++ utils/index.js
@@ -0,0 +1,11 @@
+const {createJWT,isTokenValid,attachCookiesToResponse}=require('./jwt')
+const createTokenUser=require('./createTokenUser')
+const chechPermissions=require('./checkPermissions')
+
+module.exports={
+    createJWT,
+    isTokenValid,
+    attachCookiesToResponse,
+    createTokenUser,
+    chechPermissions
+}
